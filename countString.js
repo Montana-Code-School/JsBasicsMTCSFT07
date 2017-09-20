9b7ffc62f2c23f3abfc27b1aaa2de1504cf7069f
bbalconi <bbalconi@gmail.com>
2017-09-20 16:06:48 -0600

    added countString()

--- countString.js
+++ countString.js
@@ -1,14 +1,16 @@
-//Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string.
+// Write a JavaScript function that accepts two arguments, 
+// a string and a letter and the function will count the number 
+// of occurrences of the specified letter within the string.
+//"Ummm" 'm'
+var countString = function (str, ltr) {
+  var counter = 0;
+  var lLtr = ltr.toLowerCase();
+  var strArray = str.toLowerCase().split("");
+  strArray.forEach(function (e) {
+    if (lLtr === e)
+      counter++;
+  });
+  return counter;
+};
 
-//var chai = require('chai');
-//var expect = chai.expect;
-
-
-//describe("testing out function", function(){
-//     it ('will equal a bunch of stuff', function(){
-	
-// 	var val = test([1,2,3]);
-// 	expect(val).to.equal(true);
-	
-//     });
-// });
+module.exports = countString;
