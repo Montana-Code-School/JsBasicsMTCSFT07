49c955c3eb389fc03ddcda9cd0a9c5baf03ec309
Lee Rouse <leerouse@Lees-MacBook-Pro.local>
2017-09-21 14:45:00 -0600

    added reverseStr test

--- reverse.js
+++ reverse.js
@@ -1,1 +1,10 @@
 //Write a JavaScript program to reverse a given string.
+var reverseStr = function(str){
+  var reverseToString = "";
+  if(str) {
+    var toArr = str.split("");
+    reverseToString = toArr.reverse().join("");
+  }
+  return reverseToString;
+}
+module.exports = reverseStr;
---
+++ reverse.test.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+chai.should();
+var assert = chai.assert;
+var expect = chai.expect;
+var reverseStr = require("./reverse.js");
+
+//Write a JavaScript program to reverse a given string.
+describe.only("reverseStr()", function(){
+  it('returns a string', function(){
+    var val = reverseStr('bacon');
+    assert.typeOf(val, "string");
+  })
+  it('reverses a string', function(){
+    var val = reverseStr("cat");
+    assert.equal(val, "tac");
+  })
+  it('reverses strings with capital letters', function(){
+    var val = reverseStr("Mark");
+    assert.equal(val, "kraM");
+  })
+});
