e38e2b925d83c4df666dbb1607f6d4e599ded6f5
Rob Herrmann <RobJHerrmann@gmail.com>
2017-09-21 14:44:34 -0600

    findLargest test

--- findLargest.js
+++ findLargest.js
@@ -1,1 +1,16 @@
-//Using the testing framework we just went over and TDD, write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result.
+//Using the testing framework we just went over and TDD, write a 
+//  JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result.
+
+var findLargest = function(num1, num2, num3, num4, num5){
+  var allNums = [num1, num2, num3, num4, num5];
+  var biggestNum = num1;
+  for (var i = 0; i < 5; i++) {
+    if (allNums[i] > biggestNum) {
+      biggestNum = allNums[i];
+    }
+  }
+  return biggestNum;
+};
+
+module.exports = findLargest;
+
---
+++ findLargest.test.js
@@ -0,0 +1,20 @@
+var findLargest = require('./findLargest');
+var chai = require('chai');
+chai.should();
+var assert = chai.assert;
+var expect = chai.expect;
+
+//Using the testing framework we just went over and TDD, write a 
+//  JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result.
+
+
+describe.only("findLargest", function(){
+  it ('returns a number', function(){
+    var val = findLargest(1,1,1,1,1);
+    assert.typeOf(val, 'number');;
+  });
+  it ('accepts five arguments and returns the largest', function(){
+    var val = findLargest(1,7,5,4,2);
+    assert.equal(val, 7);;
+  });
+});
