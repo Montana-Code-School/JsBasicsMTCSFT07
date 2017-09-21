f6b151f7f6f207dec8c830faaa8867ef95dbe08a
kramduckner <buckner.a.mark@gmail.com>
2017-09-21 13:37:50 -0600

    added a couple new files

---
+++ sum.js
@@ -0,0 +1,1 @@
+//Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum.
---
+++ vowels.js
@@ -0,0 +1,1 @@
+//Write a JavaScript program to count the number of vowels in a given string.
