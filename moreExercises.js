a1fd0c77a070a0f97baca5228b708426e013d341
kramduckner <buckner.a.mark@gmail.com>
2017-09-22 18:25:51 -0600

    Create moreExercises.js

---
+++ moreExercises.js
@@ -0,0 +1,27 @@
+//Using the principle of TDD and the node modules "chai" and "mocha" complete the following exercises:
+
+Given an array of integers, write a function that returns the largest integer in that array. 
+
+Given a string, write a function that returns the number of time the letter "b", "c" or "u" appear in that string.
+
+Write a JavaScript program check if a given positive number is a multiple of 3 or a multiple of 7. The function should return true or false (a boolean).
+
+Write a Javascript program that console.logs all the properties in an object. 
+
+//You don't need to write tests for the following exercises:
+
+Build an express server and serve an html file that displays the current year. Using the Web Console, console.log() the current day of the week as an intege
+of 0-6 where 0=Sunday, 1=Monday, 2=Tuesday, ... 6=Saturday.
+
+
+Next week we'll be doing more with Javascript Objects and having multiple Javascript Objects working with each other. It's important to be comfortable
+with the concept of "scope." Watch some videos (I'd be happy to recommend some), read some articles (I'd be happy to recommend some), and most importantly,
+when you're trying to learn objects and scope, WRITE SOME CODE USING THEM!!! WOOHOO!! Remember, you guys can run any javascript with node. The power is yours!
+
+Thanks guys! 
+
+See you on Monday!
+
+
+
+
