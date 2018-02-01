b61438b68a8996f1afac3c92c3f07a184328963d
kramduckner <buckner.a.mark@gmail.com>
2018-02-01 15:04:55 -0700

    Create logLetter.js

---
+++ logLetter.js
@@ -0,0 +1,16 @@
+//make the code work below by writing a function called logLetters that console logs each letter in the word "hogwaller"
+
+logLetters('hogwaller')
+
+//output - 
+//"h"
+//"o"
+//"g"
+//"w"
+//"a"
+//"l"
+//"l"
+//"e"
+//"r"
+
+//Finally, generalize the function to work with any word
