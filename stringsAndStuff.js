941e1ccf3a0b13d20faaebeb83ab06527643d50e
kramduckner <buckner.a.mark@gmail.com>
2018-02-01 15:00:12 -0700

    Create stringsAndStuff.js

---
+++ stringsAndStuff.js
@@ -0,0 +1,26 @@
+//Here's three words = moose, Christmas, a
+
+//here's a function called countLetters
+
+var countLetters = function(word){
+  
+  //write code inside these brackets that counts the number of letters in the word to make
+  //the code below work
+}
+
+
+countLetters("moose");
+// 5
+countLetters("Christmas");
+// 9
+countLetters("a");
+//1
+
+//verify that it works using console logs. If that doesn't make sense, try to figure it out!
+
+//Finally, add a console log in countLetters() that logs "We got a capital letter in this word, better get out the pitchforks
+because I hate capital letters and Mark is a moron who thinks he can get away with putting capital letters in words that my 
+function has to deal with"
+
+
+
